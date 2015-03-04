898e356d7f27baae619237c5da966c867bbdb536
Daniel <daniel.schreurs@hotmail.com>
2015-03-04 18:02:26 +0100

    add constante 'LIVETIME' qui défini la durée de vie d'un cookie.

--- config/config.php
+++ config/config.php
@@ -14,20 +14,19 @@ define('TITLE','La joie de livre');// le titre générale du site
 /*
  * La liste de vos tables afin de résoudre les éventuels
  * problèmes de noms
-
+*/
 $tables = array(
     'entite'=>'entites'
 );
-*/
+
 
 /*
  * La liste des types de fichier que l’utilisateur peut éventuellement uploader
-
+ */
 $types = array(
     'jpg'=>'image/jpeg'
 );
 
- */
 
 /*
  * Diverses constantes utiles telles que, par exemple,
@@ -51,4 +50,9 @@ define('CARBON_DIR', 'vendor/Carbon/');
  * un booléen qui tient la trace de la connexion à un espace privé
 */
  $connected = FALSE ;
- 
+
+/*
+ * définir une constante pour la durée du coikies
+ * */
+
+define('LIVETIME', time() + (2 * 3600));
