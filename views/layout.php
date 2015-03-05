f42ec01ea97c31353eec3f766db2ee7d10791194
Daniel <daniel.schreurs@hotmail.com>
2015-03-05 09:52:59 +0100

    add opérateur ? : pour savoir si la personne est connectée

--- views/layout.php
+++ views/layout.php
@@ -3,6 +3,7 @@
 <?php include(VIEW_DIR.'parts/head.php');?>
 <body>
 <?php include(VIEW_DIR.'parts/header.php'); ?>
+<?php //var_dump($controller); ?>
 <?php include($controller->view); ?>
 <?php include(VIEW_DIR.'parts/footer.php'); ?>
 <script type="text/javascript" src="./js/app.js"></script>
