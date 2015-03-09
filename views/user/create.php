e813fb8375b5d4c948c4748bcc0bb9e4148552f1
Daniel <daniel.schreurs@hotmail.com>
2015-03-09 15:51:35 +0100

    create form

---
+++ views/user/create.php
@@ -0,0 +1,32 @@
+<main>
+    <form>
+        <fieldset>
+            <legend>Qui êtes vous ?</legend>
+            <label for="first_name">Votre prénom</label>
+            <input type="text" name="first_name" id="first_name" value="" placeholder="Jane"
+                   title="Introduisez votre prénom"/>
+            <label for="last_name">Votre nom de famille</label>
+            <input type="text" name="last_name" id="last_name" value="" placeholder="Doe"
+                   title="Introduisez votre nom de famillie"/>
+        </fieldset>
+        <fieldset>
+            <legend>Votre compte</legend>
+            <label for="username">Votre nom d'utilisateur</label>
+            <input type="text" name="username" id="username" value="" title="Introduisez votre nom d’utilisateur"/>
+            <label for="email">Votre adresse mail</label>
+            <input type="email" name="email" id="email" value="" title="Introduisez votre adressse mail"/>
+            <label for="password">Votre Mot de passes</label>
+            <input type="password" name="password" id="password" value="" title="Introduisez votre mot de pass"/>
+            <label for="password1">Votre Mot de passes</label>
+            <input type="password" name="password1" id="password1" value=""
+                   title="Introduisez une seconde fois votre mot de passe"/>
+        </fieldset>
+        <fieldset>
+            <legend>Sécurité</legend>
+            <label for="question">Question de sécurité</label>
+            <input type="text" name="question" id="question" value="" title="Introduisez une question de sécurité"/>
+            <label for="reponse">réponse à cette question</label>
+            <input type="text" name="reponse" id="reponse" value="" title="Introduisez une question de sécurité"/>
+        </fieldset>
+    </form>
+</main>
