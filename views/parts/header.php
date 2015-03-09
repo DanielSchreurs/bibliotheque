efd25d0c87161732b92238ecb6077794c17cfdf4
Daniel <daniel.schreurs@hotmail.com>
2015-03-09 22:22:44 +0100

    add class

--- views/parts/header.php
+++ views/parts/header.php
@@ -10,17 +10,19 @@
             <li><span>Mon compte</span>
 
                 <div class="userLog">
-                    <?php if (!$userConnec): ?>
-                        <form action="<?php echo($_SERVER['PHP_SELF']); ?>?m=user&a=login" method="post" id="connexion"
+
+                    <?php if (!$userConnec && $data['view'] != 'create.php'): ?>
+                        <form action="<?php echo($_SERVER['PHP_SELF']); ?>?m=user&a=login" method="post"
+                              id="connexion"
                             >
                             <fieldset>
                                 <label for="username">Identifiant&nbsp;</label>
                                 <input type="text" name="username" id="username" required placeholder="Votre login">
                                 <label for="password" class="inline">Mot de passe&nbsp;</label>
-                                <a id="showPassword" class="smallInfo" href="#" title="Afficher le mot de pass">
+                                <a id="linkShowPassword" class="smallInfo" href="#" title="Afficher le mot de pass">
                                     Montrer le mot de passe</a>
                                 <input type="password" name="password" id="password" required
-                                       placeholder="Votre mot de passe">
+                                       placeholder="Votre mot de passe" class="showedpassword">
 
                                 <div>
                                     <label class="smallInfo" for="remember">Se souvenir de moi</label>
@@ -37,7 +39,11 @@
                         <a href="<?php echo($_SERVER['PHP_SELF']); ?>?m=user&a=logout" class="btnVert">Se
                             déconnecter</a>
                     <?php endif; ?>
+                    <?php if ($data['view'] == 'create.php'): ?>
+                        <a href="<?php echo($_SERVER['PHP_SELF']); ?>" class="btnVert">Retour à l'acceuil</a>
+                    <?php endif; ?>
                 </div>
+
             </li>
         </ul>
     </nav>
