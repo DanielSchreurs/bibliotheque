2c05d41aba776cfd7e1b6e8ed3dda9bcae1e184b
Daniel <daniel.schreurs@hotmail.com>
2015-03-10 12:13:44 +0100

    fixe Commit

--- models/model.php
+++ models/model.php
@@ -10,10 +10,10 @@ class model
         global $options;
         //Je me connecte au SGBD
         try {
-            $this->cx = new PDO(DSN, USERNAME, PASSWORD, $options);
+            $this->cx = new \PDO(DSN, USERNAME, PASSWORD, $options);
             $this->cx->query('SET CHARACTER SET UTF8');
             $this->cx->query('SET NAMES UTF8');
-        } catch (PDOException $e) {
+        } catch (\PDOException $e) {
             die($e->getMessage());
         }
         $this->table = $table;
