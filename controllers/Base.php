bd4aef8134a1032eaebc85557fcc6853296ec534
Daniel <daniel.schreurs@hotmail.com>
2015-03-10 13:54:07 +0100

    delete function

--- controllers/Base.php
+++ controllers/Base.php
@@ -11,14 +11,5 @@ class Base
     {
         $this->view = $request->m . '/' . $request->a . '.php';
         $this->request = $request;
-        $this->loadModel();
-    }
-
-    protected function loadModel($nomDuModel = null)
-    {
-        if (is_null($nomDuModel)) {// On passe ici à la première fois quand le model n'existe pas encore
-            $nomDuModel = '\Models\\' . ucfirst($this->request->m);
-        }
-        $this->$nomDuModel = new $nomDuModel();
     }
 }
