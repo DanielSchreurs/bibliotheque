34bcb5cf8a3d1e235853b3a785805573dbf92ae8
Daniel <daniel.schreurs@hotmail.com>
2015-03-11 20:11:45 +0100

    init file

--- helpers/html.php
+++ helpers/html.php
@@ -3,12 +3,18 @@
 /**
  *
  */
-class FilesProcessor
+namespace Helpers;
+class Html
 {
-    function __construct()
-    {
-        ;
-    }
 
+    /**
+     * @param string $a l'action(controleur)
+     * @param string $m model
+     * @return string
+     */
+    public function createLink($modele,$action,$param=null){
+        $param=http_build_query($param);
+        return $_SERVER['PHP_SELF'].'?m='.$modele.'&a='.$action.'&'.$param;
+    }
 
 }
