156844660b6b8e9b19efdacb4e1f872b89a35cd2
Daniel <daniel.schreurs@hotmail.com>
2015-03-11 20:15:43 +0100

    init file

---
+++ views/library/index.php
@@ -0,0 +1,17 @@
+<?php $html = new \Helpers\Html(); ?>
+<main>
+    <ul>
+        <?php foreach ($data['data'] as $library): ?>
+            <h3><a href="<?php echo $html->createLink('library', 'view',
+                    ['id' => $library->id]); ?>"><?php echo($library->name); ?></a></h3>
+            <h2><a href="<?php echo $html->createLink('library', 'view',
+                    ['id' => $library->id]); ?>"><?php echo($library->slogan) ?></a></h2>
+            <p><?php echo($library->phone); ?></p>
+            <a href="<?php echo($html->createLink('library', 'view', ['id' => $library->id])); ?>"><img
+                    src="./img/librarys/logo/<?php echo($library->logo); ?>.png"
+                    alt="Logo de <?php echo($library->name); ?>"/></a>
+        <?php endforeach; ?>
+        <li>
+        </li>
+    </ul>
+</main>
