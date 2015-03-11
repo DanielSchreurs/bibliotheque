02d619b443fd5e664e5ca6bb66a50b8e3afebdbc
Daniel <daniel.schreurs@hotmail.com>
2015-03-11 20:11:56 +0100

    init file

--- controllers/Page.php
+++ controllers/Page.php
@@ -2,22 +2,13 @@
 
 namespace Controllers;
 
-use Models\Book as BookModel;
 
 class Page extends Base
 {
 
-    public function index()
+    public function commentCaMarche()
     {
-        $book = new BookModel();
-        $data = $book->all();
-        $title = 'acceuil';
-        $view = 'index.php';
-        return [
-            'data' => $data,
-            'view' => $view,
-            'title' => $title
-        ];
+
     }
 
 }
