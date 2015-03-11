6b82c1335641d05ee333e4b73e83ba195b0b3062
Daniel <daniel.schreurs@hotmail.com>
2015-03-11 20:14:38 +0100

    converte to php

--- views/book/index.php
+++ views/book/index.php
@@ -1,150 +1,39 @@
 <main>
-    <form name="global" class="form_recherche">
-        <input type="search" list="tous" required placeholder="Un livre, un auteur" name="recherche" id="recherche">
-        <datalist id="tous">
-            <option value="Vincent Hein">Vincent Hein</option>
-            <option value="Joël Egloff">Joël Egloff</option>
-            <option value="Christophe Honoré">Christophe Honoré</option>
-            <option value="Thomas Paris">Thomas Paris</option>
-            <option value="Mamadou Mahmoud N'Dongo">Mamadou Mahmoud N'Dongo</option>
-            <option value="Anna Gavalda">Anna Gavalda</option>
-            <option value="Béatrice Leca">Béatrice Leca</option>
-            <option value="Johan Heliot">Johan Heliot</option>
-            <option value="Gwenaëlle Aubry">Gwenaëlle Aubry</option>
-            <option value="J.M. Erre">J.M. Erre</option>
-            <option value="Anne Godard">Anne Godard</option>
-            <option value="Samira Bellil">Samira Bellil</option>
-            <option value="Mathias Enard">Mathias Enard</option>
-            <option value="Sigrid Baffert">Sigrid Baffert</option>
-            <option value="Nicolas Fargues">Nicolas Fargues</option>
-            <option value="Laurent Gaudé">Laurent Gaudé</option>
-            <option value="Karelle Ménine">Karelle Ménine</option>
-            <option value="Valérie Mréjen">Valérie Mréjen</option>
-            <option value="Sylvain Tesson">Sylvain Tesson</option>
-            <option value="Roland Michel Tremblay">Roland Michel Tremblay</option>
-            <option value="Chloé Delaume">Chloé Delaume</option>
-        </datalist>
-        <select name="blibliotheque">
-            <option value="Chiroux" title="Selectionner votre blibliotheque">LA HESTRE(Manage)</option>
-            <option value="Chiroux" title="Selectionner votre blibliotheque">LA HULPE</option>
-            <option value="Chiroux" title="Selectionner votre blibliotheque">LA LOUVIERE</option>
-            <option value="Chiroux" title="Selectionner votre blibliotheque">La Louvière</option>
-            <option value="Chiroux" title="Selectionner votre blibliotheque">LA LOUVIERE (Boussoit)</option>
-            <option value="Chiroux" title="Selectionner votre blibliotheque">LA LOUVIERE (Haine-Saint-Pierre)</option>
-            <option value="Chiroux" title="Selectionner votre blibliotheque">LA LOUVIERE (Houdeng-Goegnies)</option>
-            <option value="Chiroux" title="Selectionner votre blibliotheque">LA LOUVIERE (Strépy-Bracquegnies)</option>
-            <option value="Chiroux" title="Selectionner votre blibliotheque">LA LOUVIERE Strépy-Bracquegnies</option>
-            <option value="Chiroux" title="Selectionner votre blibliotheque">LA REID (Theux)</option>
-            <option value="Chiroux" title="Selectionner votre blibliotheque">LAEKEN (Bruxelles)</option>
-            <option value="Chiroux" title="Selectionner votre blibliotheque">LIEGE</option>
-        </select>
-        <input type="submit" value="Rechercher !">
-    </form>
-    <nav class="main_nav_Bibli_Small">
-        <ul>
-            <li><a href="#" title="affiche tous les auteurs">tous les auteurs</a></li>
-            <li><a href="#" title="affiche tous les editeurs">tous les editeurs</a></li>
-            <li><a href="#" title="affiche tous les genres">tous les genres</a></li>
-            <li><a href="#" title="affiche tous les annees">tous les annees</a></li>
-        </ul>
-    </nav>
+    <?php include('./views/parts/form_recherche.php'); ?>
+    <?php include('./views/parts/main_nav_bibli.php'); ?>
     <h2>Derniers ajoutés</h2>
     <ul class="liste_livres">
-        <li>
-            <h3><a href="#" title="Renvois vers une page avec uniquement le livre Anthologie de l'humour noir">Anthologie de l'humour noir</a></h3><img src="./img/andreBreton/andre_breton_anthologie_de_lhumour_noir.jpg" alt="première du livreAnthologie de l'humour noir" width="180" height="270">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur metus neque, semper a mattis tristique, tempor non turpis. Ut enim diam, venenatis non ipsum at, placerat dictum nisi. Vestibulum finibus, nulla nec molestie aliquam, libero augue mattis elit, sed cursus nisl purus ac mauris. Phasellus eget lectus augue. Donec vel risus orci. Phasellus ut ultrices ligula. Curabitur sodales dolor vel lacus sollicitudin, sit amet tempor nulla volutpattristique, tempor non turpis. Ut enim diam, venenatis non ipsum at, placerat dictum nisi. Vestibulum finibus, nulla nec molestie aliquam, libero augue mattis elit, sed cursus nisl purus ac mauris. Phasellus eget lectus augue. Donec vel risus orci. Phasellus ut ultrices ligula. Curabitur sodales dolor vel lacus sollicitudin, sit amet tempor nulla volutpat</p>
-            <dl class="info_Livre clearfix">
-                <dt>Auteur</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Auteurs de François">François</a></dd>
-                <dt>Editeur</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Editeurs de Livre de poche">Livre de poche</a></dd>
-                <dt>Genre</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Genres de culture">culture</a></dd>
-                <dt>Année</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Années de 2015">2015</a></dd>
-                <dt>Bibliothèque</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Bibliothèques de Chiroux">Chiroux</a></dd>
-            </dl>
-        </li>
-        <li>
-            <h3><a href="#" title="Renvois vers une page avec uniquement le livre Anthologie de l'humour noir">Anthologie de l'humour noir</a></h3><img src="./img/andreBreton/andre_breton_anthologie_de_lhumour_noir.jpg" alt="première du livreAnthologie de l'humour noir" width="180" height="270">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur metus neque, semper a mattis tristique, tempor non turpis. Ut enim diam, venenatis non ipsum at, placerat dictum nisi. Vestibulum finibus, nulla nec molestie aliquam, libero augue mattis elit, sed cursus nisl purus ac mauris. Phasellus eget lectus augue. Donec vel risus orci. Phasellus ut ultrices ligula. Curabitur sodales dolor vel lacus sollicitudin, sit amet tempor nulla volutpattristique, tempor non turpis. Ut enim diam, venenatis non ipsum at, placerat dictum nisi. Vestibulum finibus, nulla nec molestie aliquam, libero augue mattis elit, sed cursus nisl purus ac mauris. Phasellus eget lectus augue. Donec vel risus orci. Phasellus ut ultrices ligula. Curabitur sodales dolor vel lacus sollicitudin, sit amet tempor nulla volutpat</p>
-            <dl class="info_Livre clearfix">
-                <dt>Auteur</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Auteurs de François">François</a></dd>
-                <dt>Editeur</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Editeurs de Livre de poche">Livre de poche</a></dd>
-                <dt>Genre</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Genres de culture">culture</a></dd>
-                <dt>Année</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Années de 2015">2015</a></dd>
-                <dt>Bibliothèque</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Bibliothèques de Chiroux">Chiroux</a></dd>
-            </dl>
-        </li>
-        <li>
-            <h3><a href="#" title="Renvois vers une page avec uniquement le livre Anthologie de l'humour noir">Anthologie de l'humour noir</a></h3><img src="./img/andreBreton/andre_breton_anthologie_de_lhumour_noir.jpg" alt="première du livreAnthologie de l'humour noir" width="180" height="270">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur metus neque, semper a mattis tristique, tempor non turpis. Ut enim diam, venenatis non ipsum at, placerat dictum nisi. Vestibulum finibus, nulla nec molestie aliquam, libero augue mattis elit, sed cursus nisl purus ac mauris. Phasellus eget lectus augue. Donec vel risus orci. Phasellus ut ultrices ligula. Curabitur sodales dolor vel lacus sollicitudin, sit amet tempor nulla volutpattristique, tempor non turpis. Ut enim diam, venenatis non ipsum at, placerat dictum nisi. Vestibulum finibus, nulla nec molestie aliquam, libero augue mattis elit, sed cursus nisl purus ac mauris. Phasellus eget lectus augue. Donec vel risus orci. Phasellus ut ultrices ligula. Curabitur sodales dolor vel lacus sollicitudin, sit amet tempor nulla volutpat</p>
-            <dl class="info_Livre clearfix">
-                <dt>Auteur</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Auteurs de François">François</a></dd>
-                <dt>Editeur</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Editeurs de Livre de poche">Livre de poche</a></dd>
-                <dt>Genre</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Genres de culture">culture</a></dd>
-                <dt>Année</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Années de 2015">2015</a></dd>
-                <dt>Bibliothèque</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Bibliothèques de Chiroux">Chiroux</a></dd>
-            </dl>
-        </li>
-        <li>
-            <h3><a href="#" title="Renvois vers une page avec uniquement le livre Anthologie de l'humour noir">Anthologie de l'humour noir</a></h3><img src="./img/andreBreton/andre_breton_anthologie_de_lhumour_noir.jpg" alt="première du livreAnthologie de l'humour noir" width="180" height="270">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur metus neque, semper a mattis tristique, tempor non turpis. Ut enim diam, venenatis non ipsum at, placerat dictum nisi. Vestibulum finibus, nulla nec molestie aliquam, libero augue mattis elit, sed cursus nisl purus ac mauris. Phasellus eget lectus augue. Donec vel risus orci. Phasellus ut ultrices ligula. Curabitur sodales dolor vel lacus sollicitudin, sit amet tempor nulla volutpattristique, tempor non turpis. Ut enim diam, venenatis non ipsum at, placerat dictum nisi. Vestibulum finibus, nulla nec molestie aliquam, libero augue mattis elit, sed cursus nisl purus ac mauris. Phasellus eget lectus augue. Donec vel risus orci. Phasellus ut ultrices ligula. Curabitur sodales dolor vel lacus sollicitudin, sit amet tempor nulla volutpat</p>
-            <dl class="info_Livre clearfix">
-                <dt>Auteur</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Auteurs de François">François</a></dd>
-                <dt>Editeur</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Editeurs de Livre de poche">Livre de poche</a></dd>
-                <dt>Genre</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Genres de culture">culture</a></dd>
-                <dt>Année</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Années de 2015">2015</a></dd>
-                <dt>Bibliothèque</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Bibliothèques de Chiroux">Chiroux</a></dd>
-            </dl>
-        </li>
-        <li>
-            <h3><a href="#" title="Renvois vers une page avec uniquement le livre Anthologie de l'humour noir">Anthologie de l'humour noir</a></h3><img src="./img/andreBreton/andre_breton_anthologie_de_lhumour_noir.jpg" alt="première du livreAnthologie de l'humour noir" width="180" height="270">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur metus neque, semper a mattis tristique, tempor non turpis. Ut enim diam, venenatis non ipsum at, placerat dictum nisi. Vestibulum finibus, nulla nec molestie aliquam, libero augue mattis elit, sed cursus nisl purus ac mauris. Phasellus eget lectus augue. Donec vel risus orci. Phasellus ut ultrices ligula. Curabitur sodales dolor vel lacus sollicitudin, sit amet tempor nulla volutpattristique, tempor non turpis. Ut enim diam, venenatis non ipsum at, placerat dictum nisi. Vestibulum finibus, nulla nec molestie aliquam, libero augue mattis elit, sed cursus nisl purus ac mauris. Phasellus eget lectus augue. Donec vel risus orci. Phasellus ut ultrices ligula. Curabitur sodales dolor vel lacus sollicitudin, sit amet tempor nulla volutpat</p>
-            <dl class="info_Livre clearfix">
-                <dt>Auteur</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Auteurs de François">François</a></dd>
-                <dt>Editeur</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Editeurs de Livre de poche">Livre de poche</a></dd>
-                <dt>Genre</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Genres de culture">culture</a></dd>
-                <dt>Année</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Années de 2015">2015</a></dd>
-                <dt>Bibliothèque</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Bibliothèques de Chiroux">Chiroux</a></dd>
-            </dl>
-        </li>
-        <li>
-            <h3><a href="#" title="Renvois vers une page avec uniquement le livre Anthologie de l'humour noir">Anthologie de l'humour noir</a></h3><img src="./img/andreBreton/andre_breton_anthologie_de_lhumour_noir.jpg" alt="première du livreAnthologie de l'humour noir" width="180" height="270">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur metus neque, semper a mattis tristique, tempor non turpis. Ut enim diam, venenatis non ipsum at, placerat dictum nisi. Vestibulum finibus, nulla nec molestie aliquam, libero augue mattis elit, sed cursus nisl purus ac mauris. Phasellus eget lectus augue. Donec vel risus orci. Phasellus ut ultrices ligula. Curabitur sodales dolor vel lacus sollicitudin, sit amet tempor nulla volutpattristique, tempor non turpis. Ut enim diam, venenatis non ipsum at, placerat dictum nisi. Vestibulum finibus, nulla nec molestie aliquam, libero augue mattis elit, sed cursus nisl purus ac mauris. Phasellus eget lectus augue. Donec vel risus orci. Phasellus ut ultrices ligula. Curabitur sodales dolor vel lacus sollicitudin, sit amet tempor nulla volutpat</p>
-            <dl class="info_Livre clearfix">
-                <dt>Auteur</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Auteurs de François">François</a></dd>
-                <dt>Editeur</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Editeurs de Livre de poche">Livre de poche</a></dd>
-                <dt>Genre</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Genres de culture">culture</a></dd>
-                <dt>Année</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Années de 2015">2015</a></dd>
-                <dt>Bibliothèque</dt>
-                <dd><a href="#" title="Renvois vers une page qui reprend tous les Bibliothèques de Chiroux">Chiroux</a></dd>
-            </dl>
-        </li>
+        <?php foreach ($data['data'] as $book): ?>
+            <li>
+                <h3><a href="#"
+                       title="Renvois vers une page avec uniquement le livre Anthologie de <?php echo($book->title); ?>"><?php echo($book->title); ?></a>
+                </h3><img src="./img/books_covers/<?php echo($book->front_cover); ?>.jpg"
+                          alt="première du <?php echo($book->title); ?>" width="180" height="270">
+
+                <p><?php echo($book->summary); ?></p>
+                <dl class="info_Livre clearfix">
+                    <dt>Auteur</dt>
+                    <dd><a href="#"
+                           title="Renvois vers une page qui reprend tous les livre de <?php echo($book->author_first_name . ' ' . $book->author_last_name); ?>"><?php echo($book->author_first_name . ' ' . $book->author_last_name); ?></a>
+                    </dd>
+                    <dt>Editeur</dt>
+                    <dd><a href="#"
+                           title="Renvois vers une page qui reprend tous de la maison d'édition, <?php echo($book->editor_name); ?>"><?php echo($book->editor_name); ?></a>
+                    </dd>
+                    <dt>Genre</dt>
+                    <dd><a href="#"
+                           title="Renvois vers une page qui reprend tous les livre du genre <?php echo($book->genre_name); ?>"><?php echo($book->genre_name); ?></a>
+                    </dd>
+                    <dt>Année</dt>
+                    <dd><a href="#"
+                           title="Renvois vers une page qui reprend tous les livre de l'année <?php echo($book->datepub); ?>"><?php echo(date('Y',strtotime($book->datepub))); ?></a>
+                    </dd>
+                    <dt>Bibliothèque</dt>
+                    <dd><a href="#"
+                           title="Renvois vers une page qui reprend tous les Bibliothèques de la bibliothèque <?php echo($book->library_name); ?>"><?php echo($book->library_name); ?></a>
+                    </dd>
+                </dl>
+            </li>
+        <?php endforeach; ?>
     </ul>
 </main>
