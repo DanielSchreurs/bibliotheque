a5abcc2f97eccbb12f6650d9c77b3be7b12ade5a
Daniel <daniel.schreurs@hotmail.com>
2015-03-11 23:03:34 +0100

    add link

--- views/author/index.php
+++ views/author/index.php
@@ -1,8 +1,9 @@
+<?php $html= new \Helpers\Html();?>
 <main>
     <ul>
         <?php foreach($data['data'] as $author): ?>
         <li>
-            <h3><a href="<?php echo($_SERVER['PHP_SELF']) ?>?m=author&a=view&id=<?php echo($author->author_id); ?>"><?php echo($author->first_name.' '.$author->last_name) ?></a></h3>
+            <h3><a href="<?php echo($html->createLink('book','find',['id'=>$author->author_id])); ?>"><?php echo($author->first_name.' '.$author->last_name) ?></a></h3>
             <img src="./img/authors_photo/logo/<?php echo($author->logo); ?>" alt="Image de <?php echo($author->first_name.' '.$author->last_name);?> "/>
             <p><?php echo(mb_substr($author->bio_text,0,200)); ?><a href="<?php echo($_SERVER['PHP_SELF']) ?>?m=author&a=view&id=<?php echo($author->author_id); ?>">Lire la suite</a></p>
             <a href="<?php echo($_SERVER['PHP_SELF']);?>?m=book&a=view&id=<?php echo($author->author_id);?>"></a>
