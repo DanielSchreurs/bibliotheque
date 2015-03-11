bc683be4dc035164fadee437f1e6b4bd616ba2c7
Daniel <daniel.schreurs@hotmail.com>
2015-03-11 20:12:22 +0100

    add picture

---
+++ controllers/Library.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danielschreurs
+ */
+
+namespace Controllers;
+
+use Models\Library as ModelLibrary;
+
+class Library extends Base
+{
+    function __construct($request)
+    {
+        parent::__construct($request);
+        $this->modelLibrary = new ModelLibrary();
+    }
+
+    public function index()
+    {
+
+        $data = $this->modelLibrary->all();
+        $view = 'index.php';
+        $title = 'Nos bibliothèques';
+        return [
+            'data' => $data,
+            'view' => $view,
+            'title' => $title
+        ];
+    }
+}
