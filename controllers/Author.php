27ef5074364a47d5e1cf0dcf30a43584aa7ea7dd
Daniel <daniel.schreurs@hotmail.com>
2015-03-11 16:30:57 +0100

    add index and view function

--- controllers/Author.php
+++ controllers/Author.php
@@ -4,18 +4,41 @@
  *
  */
 namespace Controllers;
-class AuthorController extends BaseController
+
+use Models\Author as ModelAuthor;
+
+class Author extends Base
 {
-    function __construct()
+    function __construct($request)
     {
-        ;
+        parent::__construct($request);
+        $this->modelAuthor = new ModelAuthor();
     }
 
     function index()
     {
 
+        $data = $this->modelAuthor->all();
+        $title = 'Les auteurs';
+        $view = 'index.php';
+        return [
+            'data' => $data,
+            'view' => $view,
+            'title' => $title
+        ];
     }
 
+    public function view (){
+        $user = new ModelAuthor();
+        $data = $user->find($this->request->id);
+        $title = 'Les auteurs';
+        $view = 'index.php';
+        return [
+            'data' => $data,
+            'view' => $view,
+            'title' => $title
+        ];
+    }
     function show()
     {
 
