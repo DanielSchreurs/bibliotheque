c44b77b6119ba76fd8bbec564cdf2fed62e97769
Daniel <daniel.schreurs@hotmail.com>
2015-03-13 06:09:24 +0100

    add HTML5

--- views/author/index.php
+++ views/author/index.php
@@ -1,12 +1,12 @@
 <?php $html= new \Helpers\Html();?>
 <main>
-    <ul>
+    <ul class="liste_Bibliotheques">
         <?php foreach($data['data'] as $author): ?>
         <li>
-            <h3><a href="<?php echo($html->createLink('book','find',['id'=>$author->author_id])); ?>"><?php echo($author->first_name.' '.$author->last_name) ?></a></h3>
-            <img src="./img/authors_photo/logo/<?php echo($author->logo); ?>" alt="Image de <?php echo($author->first_name.' '.$author->last_name);?> "/>
-            <p><?php echo(mb_substr($author->bio_text,0,200)); ?><a href="<?php echo($_SERVER['PHP_SELF']) ?>?m=author&a=view&id=<?php echo($author->author_id); ?>">Lire la suite</a></p>
-            <a href="<?php echo($_SERVER['PHP_SELF']);?>?m=book&a=view&id=<?php echo($author->author_id);?>"></a>
+            <h3><a href="<?php echo($html->createLink('author','view',['id'=>$author->author_id])); ?>"><?php echo($author->first_name.' '.$author->last_name) ?></a></h3>
+            <img src="./img/authors_photo/logo/<?php echo($author->logo.'.png'); ?>" alt="Image de <?php echo($author->first_name.' '.$author->last_name);?> "/>
+            <p><?php echo($html->cutText($author->bio_text,200)); ?></p>
+            <a class="btnVert" href="<?php echo($html->createLink('author','view',['id'=>$author->author_id]));?>">Voir la fiche de l'auteur</a>
         </li>
         <?php endforeach; ?>
     </ul>
--- views/parts/header.php
+++ views/parts/header.php
@@ -1,47 +1,52 @@
 <header>
-    <h1><a href="<?php echo($_SERVER['PHP_SELF']) ?>"><?php echo(TITLE); ?></a></h1>
+    <a class="box_logo" href="<?php echo($_SERVER['PHP_SELF']) ?>"><?php echo(TITLE); ?></a>
     <nav class="header_nav">
-        <ul>
-            <li><a href="<?php echo($_SERVER['PHP_SELF']); ?>" title="Renvois vers la page d'accueil">accueil</a></li>
-            <li><a href="<?php  echo($_SERVER['PHP_SELF']);?>?m=library&a=index" title="Renvois vers la page Nos blibliothèques">Nos
-                    blibliothèques</a></li>
-            <li><a href="./vues/public/commentMarche.php" title="Renvois vers la page Comment ça marche">Comment ça
-                    marche</a></li>
-            <li><span>Mon compte</span>
-                <div class="userLog">
+        <h2 class="hidden">Menu de navigation</h2>
+        <a class="nav_items" href="<?php echo($_SERVER['PHP_SELF']); ?>"
+           title="Renvois vers la page d'accueil">accueil</a>
+        <a class="nav_items" href="<?php echo($_SERVER['PHP_SELF']); ?>?m=library&a=index"
+               title="Renvois vers la page Nos blibliothèques">Nos
+                blibliothèques</a>
+        <a class="nav_items" href="./vues/public/commentMarche.php" title="Renvois vers la page Comment ça marche">Comment
+                ça
+                marche</a>
+        <div class="show_form_connexion">
+            <span class="nav_items">Mon compte</span>
 
-                    <?php if (!$userConnec && $data['view'] != 'create.php'): ?>
-                        <form action="<?php echo($_SERVER['PHP_SELF']); ?>?m=user&a=login" method="post"
-                              id="connexion"
-                            >
-                            <fieldset>
-                                <label for="username">Identifiant&nbsp;</label>
-                                <input type="text" name="username" id="username" required placeholder="Votre login">
-                                <label for="password" class="inline">Mot de passe&nbsp;</label>
-                                <a id="linkShowPassword" class="smallInfo" href="#" title="Afficher le mot de pass">
-                                    Montrer le mot de passe</a>
-                                <input type="password" name="password" id="password" required
-                                       placeholder="Votre mot de passe" class="showedpassword">
-                                <div>
-                                    <label class="smallInfo" for="remember">Se souvenir de moi</label>
-                                    <input type="checkbox" name="remember" value="remember" id="remember"/>
-                                </div>
-                                <a href="<?php echo($_SERVER['PHP_SELF']); ?>?m=user&a=create"
-                                   class="btnVert">S'incrire</a><input
-                                    class="btnVert" type="submit" value="Se connecter">
-                            </fieldset>
-                        </form>
-                    <?php endif; ?>
-                    <?php if ($userConnec): ?>
-                        <p>Bonjour <?php echo($_COOKIE['first_name']); ?></p>
-                        <a href="<?php echo($_SERVER['PHP_SELF']); ?>?m=user&a=logout" class="btnVert">Se
-                            déconnecter</a>
-                    <?php endif; ?>
-                    <?php if ($data['view'] == 'create.php'): ?>
-                        <a href="<?php echo($_SERVER['PHP_SELF']); ?>" class="btnVert">Retour à l'acceuil</a>
-                    <?php endif; ?>
-                </div>
-            </li>
-        </ul>
+            <div class="userLog">
+                <?php if (!$userConnec && $data['view'] != 'create.php'): ?>
+                    <form action="<?php echo($_SERVER['PHP_SELF']); ?>?m=user&a=login" method="post"
+                          id="connexion"
+                        >
+                        <fieldset>
+                            <label for="username">Identifiant&nbsp;</label>
+                            <input type="text" name="username" id="username" required placeholder="Votre login">
+                            <label for="password" class="inline">Mot de passe&nbsp;</label>
+                            <a id="linkShowPassword" class="smallInfo" href="#" title="Afficher le mot de pass">
+                                Montrer le mot de passe</a>
+                            <input type="password" name="password" id="password" required
+                                   placeholder="Votre mot de passe" class="showedpassword">
+
+                            <div>
+                                <label class="smallInfo" for="remember">Se souvenir de moi</label>
+                                <input type="checkbox" name="remember" value="remember" id="remember"/>
+                            </div>
+                            <a href="<?php echo($_SERVER['PHP_SELF']); ?>?m=user&a=create"
+                               class="btnVert">S'incrire</a><input
+                                class="btnVert" type="submit" value="Se connecter">
+                        </fieldset>
+                    </form>
+                <?php endif; ?>
+                <?php if ($userConnec): ?>
+                    <p>Bonjour <?php echo($_COOKIE['first_name']); ?></p>
+                    <a href="<?php echo($_SERVER['PHP_SELF']); ?>?m=user&a=logout" class="btnVert">Se
+                        déconnecter</a>
+                <?php endif; ?>
+                <?php if ($data['view'] == 'create.php'): ?>
+                    <a href="<?php echo($_SERVER['PHP_SELF']); ?>" class="btnVert">Retour à l'acceuil</a>
+                <?php endif; ?>
+            </div>
+        </div>
+
     </nav>
 </header>
