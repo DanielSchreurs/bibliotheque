a263da2b31906cf3c5008929bd2e6edd600b54ac
Daniel <daniel.schreurs@hotmail.com>
2015-03-13 06:09:11 +0100

    fix function

--- helpers/html.php
+++ helpers/html.php
@@ -4,21 +4,19 @@
  *
  */
 namespace Helpers;
+
 class Html
 {
 
-    /**
-     * @param string $a l'action(controleur)
-     * @param string $m model
-     * @return string
-     */
-    public function createLink($modele,$action,$param=null){
-        $param=http_build_query($param);
-        return $_SERVER['PHP_SELF'].'?m='.$modele.'&a='.$action.'&'.$param;
+    public function createLink($modele, $action, $param = null)
+    {
+        $param = http_build_query($param);
+        return $_SERVER['PHP_SELF'] . '?m=' . $modele . '&a=' . $action . '&' . $param;
     }
 
-    public function cutText($chaine,$long){
-        return explode('\break',wordwrap($chaine,$long,'\break'))[0].'1&nbsp;&hellip;';
+    public function cutText($chaine, $long)
+    {
+        return explode('\break', wordwrap($chaine, $long, '\break'))[0] . '1&nbsp;&hellip;';
     }
 
 }
