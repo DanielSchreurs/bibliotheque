f929b47beb4ce7d5e2b4ebf5a2a53912c2f3b0e1
Daniel <daniel.schreurs@hotmail.com>
2015-03-14 13:13:11 +0100

    change values

--- views/author/view.php
+++ views/author/view.php
@@ -10,8 +10,8 @@
         </h2>
         <a class="float_left" href="<?php echo($html->createLink('book', 'view', ['id' => $author->id])); ?>"><img
                 src="./img/authors_photos/<?php echo($author->photo); ?>.jpg"
-                alt="photo de  <?php echo($author->first_name . ' ' . $author->first_name); ?>" width="180"
-                height="270"></a>
+                alt="photo de  <?php echo($author->first_name . ' ' . $author->first_name); ?>" width="300"
+                height="450"></a>
 
         <p class=""><?php echo($author->bio_text); ?> </p>
 
