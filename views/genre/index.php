edb642dcd9ec1daaeb485aa1cc4a87d9fc14c9f1
Daniel <daniel.schreurs@hotmail.com>
2015-03-15 20:17:49 +0100

    init file

---
+++ views/genre/index.php
@@ -0,0 +1,11 @@
+<main>
+    <?php include('./views/parts/form_recherche.php'); ?>
+    <?php include('./views/parts/main_nav_bibli.php'); ?>
+    <ul class="liste_genre">
+        <?php foreach($data['data'] as $genre): ?>
+            <li>
+                <h3><a href="<?php echo($html->createLink('genre','view',['id'=>$genre->id])); ?>"><?php echo($genre->name) ?></a></h3>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</main>
