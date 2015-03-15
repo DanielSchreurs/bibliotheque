d305d527d6e1c4fb10fa1dacd99fe6483b759a38
Daniel <daniel.schreurs@hotmail.com>
2015-03-15 22:33:58 +0100

    init file

--- models/genre.php
+++ models/genre.php
@@ -14,11 +14,30 @@ class Genre extends Model
     public function all()
     {
         $sql = '
-              SELECT
+               SELECT
               name,
               id
-              FROM genres';
+              FROM genres
+              ORDER BY name';
         $pdost=$this->cx->query($sql);
+        return $pdost->fetchAll();
+    }
+
+    public function find($id_genre)
+    {
+        $sql='
+              SELECT
+              name AS genre_name,
+              genres.id AS genre_id,
+              books.id AS book_id,
+              summary,
+              title AS book_title,
+              logo AS book_cover
+              FROM genres
+              LEFT JOIN books on genres.id=genre_id
+              WHERE genres.id=:id_genre';
+        $pdost=$this->cx->prepare($sql);
+        $pdost->execute(['id_genre'=>$id_genre]);
         return $pdost->fetchAll();
     }
 }
