f197e1f2bb39c415a996a019fdc1816a562eb6a9
Daniel <daniel.schreurs@hotmail.com>
2015-03-15 20:17:58 +0100

    init file

--- models/genre.php
+++ models/genre.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Models;
+
 class Genre extends Model
 {
     protected $table = 'genres';
@@ -8,5 +9,16 @@ class Genre extends Model
     function __construct()
     {
         parent::__construct($this->table);
+    }
+
+    public function all()
+    {
+        $sql = '
+              SELECT
+              name,
+              id
+              FROM genres';
+        $pdost=$this->cx->query($sql);
+        return $pdost->fetchAll();
     }
 }
