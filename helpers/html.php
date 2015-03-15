488673d1b86779bbaeb0da1dab7da9637d791bb5
Daniel <daniel.schreurs@hotmail.com>
2015-03-15 12:53:34 +0100

    add function

--- helpers/html.php
+++ helpers/html.php
@@ -13,6 +13,7 @@ class Html
         $param = http_build_query($param);
         return $_SERVER['PHP_SELF'] . '?m=' . $modele . '&a=' . $action . '&' . $param;
     }
+
     public function cutText($chaine, $long)
     {
         return explode('\break', wordwrap($chaine, $long, '\break'))[0] . '&nbsp;&hellip;';
@@ -20,10 +21,47 @@ class Html
 
     public function dateToSTring($date)
     {
-        $date=strtotime($date);
-        $jours=['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche'];
-        $mois=['janvier','février','mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre'];
-        return 'le '.$jours[date('N',$date)-1].' '.date('j',$date).' '.$mois[(date('n',$date))-1].' à '.date('h',$date).':'.date('i',$date);
+        $date = strtotime($date);
+        $jours = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+        $mois = [
+            'janvier',
+            'février',
+            'mars',
+            'avril',
+            'mai',
+            'juin',
+            'juillet',
+            'août',
+            'septembre',
+            'octobre',
+            'novembre',
+            'décembre'
+        ];
+        return 'le ' . $jours[date('N', $date) - 1] . ' ' . date('j', $date) . ' ' . $mois[(date('n',
+            $date)) - 1] . ' à ' . date('h', $date) . ':' . date('i', $date);
+    }
+
+    public function birthToString($date)
+    {
+        $date = strtotime($date);
+        $mois = [
+            'janvier',
+            'février',
+            'mars',
+            'avril',
+            'mai',
+            'juin',
+            'juillet',
+            'août',
+            'septembre',
+            'octobre',
+            'novembre',
+            'décembre'
+        ];
+
+        return date('j', $date). ' ' . $mois[date('m', $date)-1] . ' ' . date('Y', $date);
+
+        
     }
 
 }
