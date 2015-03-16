6e9077d7b263e525453a3c640291348cb2506ee8
Daniel <daniel.schreurs@hotmail.com>
2015-03-16 22:43:41 +0100

    Honte à moi

--- views/book/view.php
+++ views/book/view.php
@@ -5,7 +5,7 @@
     <article class="single_book">
         <h2>
             <a href="<?php echo($html->createLink('book', 'view', ['id' => $book->book_id])); ?>"
-               title="Renvois vers une page avec uniquement le livre Anthologie de <?php echo($book->title); ?>"><?php echo($book->title); ?></a>
+               title="Renvois vers une page avec uniquement le livres Anthologie de <?php echo($book->title); ?>"><?php echo($book->title); ?></a>
         </h2>
         <a class="float_left" href="<?php echo($html->createLink('book', 'view', ['id' => $book->book_id])); ?>"><img
                 src="./img/books_covers/<?php echo($book->front_cover); ?>.jpg"
@@ -15,7 +15,7 @@
         <dl class="info_Livre clearfix">
             <dt>Auteur</dt>
             <dd><a href="<?php echo($html->createLink('author', 'view', ['id' => $book->author_id])); ?>"
-                   title="Renvois vers une page qui reprend tous les livre de <?php echo($book->author_first_name . ' ' . $book->author_last_name); ?>"><?php echo($book->author_first_name . ' ' . $book->author_last_name); ?></a>
+                   title="Renvois vers une page qui reprend tous les livres de <?php echo($book->author_first_name . ' ' . $book->author_last_name); ?>"><?php echo($book->author_first_name . ' ' . $book->author_last_name); ?></a>
             </dd>
             <dt>Editeur</dt>
             <dd><a href="<?php echo($html->createLink('editor', 'view', ['id' => $book->editor_id])); ?>"
@@ -23,11 +23,11 @@
             </dd>
             <dt>Genre</dt>
             <dd><a href="<?php echo($html->createLink('genre', 'view', ['id' => $book->genre_id])); ?>"
-                   title="Renvois vers une page qui reprend tous les livre du genre <?php echo($book->genre_name); ?>"><?php echo($book->genre_name); ?></a>
+                   title="Renvois vers une page qui reprend tous les livres du genre <?php echo($book->genre_name); ?>"><?php echo($book->genre_name); ?></a>
             </dd>
             <dt>Année</dt>
             <dd><a href="<?php //echo($html->createLink('book','view',['id'=>$book->editor_id]));?>"
-                   title="Renvois vers une page qui reprend tous les livre de l'année <?php echo($book->datepub); ?>"><?php echo(date('Y',
+                   title="Renvois vers une page qui reprend tous les livres de l'année <?php echo($book->datepub); ?>"><?php echo(date('Y',
                         strtotime($book->datepub))); ?></a>
             </dd>
             <dt>Bibliothèque</dt>
