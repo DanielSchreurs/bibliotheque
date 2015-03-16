f3ba2fb9815727f8ab1507a160ff2351e2283626
Daniel <daniel.schreurs@hotmail.com>
2015-03-16 20:59:08 +0100

    init HTML file

---
+++ views/page/help.php
@@ -0,0 +1,26 @@
+<main>
+    <?php include('./views/parts/form_recherche.php'); ?>
+<?php include('./views/parts/main_nav_bibli.php'); ?>
+    <h2>Comment ça marche&nbsp;?</h2>
+    <ul class="liste_Questions">
+        <li>
+            <h3><a href="#" title="">Comment faire une recherche ?</a></h3>
+            <blockquote>
+                <p>Il suffit d’introduire dans la bare de recherche principale le livre que vous recherchez.</p>
+            </blockquote>
+        </li>
+        <li>
+            <h3><a href="#" title="">Pourquoi mon livre n’apprait-il pas ?</a></h3>
+            <blockquote>
+                <p>Nous ne référencions que les livres qui sont repris dans l’inventaire des bibliothèques qui meublent ce site. Si le livre n’apparait pas, c’est qu’aucune bibliothèque ne semble l’avoir.</p>
+            </blockquote>
+        </li>
+        <li>
+            <h3><a href="#" title="">Pourquoi me demande-t-on d’indiquer ma région ?</a></h3>
+            <blockquote>
+                <p>De cette façon, nous pouvons organiser la liste des bibliothèques qui disposent d’un livre recherché en fonction de votre région. Pas de Big brother, juste un soucis d’économies de temps, de carburant et de bonne humeur !</p>
+            </blockquote>
+        </li>
+    </ul>
+
+</main>
