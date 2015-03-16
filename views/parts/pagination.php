d1e39a521380aadaf0dd3f7a627073b834e3ae43
Daniel <daniel.schreurs@hotmail.com>
2015-03-16 22:43:50 +0100

    rename

--- views/parts/pagination.php
+++ views/parts/pagination.php
@@ -1,10 +1,10 @@
 <ul class="pagination">
-    <li class="page_number"><span class="greyButton">1</span></li>
-    <li class="page_number"><a class="greyButton" href="<?php echo($html->createLink('book','group')) ?>">2</a></li>
-    <li class="page_number"><a class="greyButton" href="<?php echo($html->createLink('book','group')) ?>">3</a></li>
-    <li class="page_number"><a class="greyButton" href="<?php echo($html->createLink('book','group')) ?>">4</a></li>
-    <li class="page_number"><a class="greyButton" href="<?php echo($html->createLink('book','group')) ?>">5</a></li>
-    <li class="page_dots"><span class="greyButton"> ... </span></li>
-    <li class="page_next_set"><a class="greyButton" href="<?php echo($html->createLink('book','group')) ?>0">10</a></li>
-    <li class="page_next"><a href="<?php echo($html->createLink('book','group')) ?>" class="btn_large">Suivant</a></li>
+    <li class="page_number"><span class="numberBtn large">1</span></li>
+    <li class="page_number"><a class="numberBtn" href="<?php echo($html->createLink('book','group')) ?>">2</a></li>
+    <li class="page_number"><a class="numberBtn" href="<?php echo($html->createLink('book','group')) ?>">3</a></li>
+    <li class="page_number"><a class="numberBtn" href="<?php echo($html->createLink('book','group')) ?>">4</a></li>
+    <li class="page_number"><a class="numberBtn" href="<?php echo($html->createLink('book','group')) ?>">5</a></li>
+    <li class="page_number"><span class="numberBtn"> ... </span></li>
+    <li class="page_number"><a class="numberBtn" href="<?php echo($html->createLink('book','group')) ?>0">10</a></li>
+    <li class="page_number"><a href="<?php echo($html->createLink('book','group')) ?>" class="numberBtn large">Suivant</a></li>
 </ul>
