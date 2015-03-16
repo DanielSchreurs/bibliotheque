9038fa8e2282284091cec66bd1a7de043f2b8b19
Daniel <daniel.schreurs@hotmail.com>
2015-03-16 18:53:49 +0100

    init file

---
+++ views/year/index.php
@@ -0,0 +1,12 @@
+<main>
+    <?php include('./views/parts/form_recherche.php'); ?>
+    <?php include('./views/parts/main_nav_bibli.php'); ?>
+    <ul class="liste_genre">
+        <?php foreach($data['data'] as $annee): ;
+        ?>
+            <li>
+                <h3><a href="<?php echo($html->createLink('book','liste',['id'=>$annee->year])); ?>"><?php echo($annee->year) ?></a></h3>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</main>
