8ad5d4225547aa91204c54e8128b98100cc62cd2
Daniel <daniel.schreurs@hotmail.com>
2015-03-17 20:59:04 +0100

    add sql

--- models/book.php
+++ models/book.php
@@ -24,9 +24,10 @@ class Book extends Model implements BookRepositoryInterface
      */
 
     public function all()
-    {//j'écrase la method 'all' de Model
+    {
+
         $sql = '
-                SELECT
+                 SELECT
                 books.id AS book_id,
                 genres.id AS genre_id,
                 editors.id AS editor_id,
@@ -49,7 +50,43 @@ class Book extends Model implements BookRepositoryInterface
                 JOIN editors on editor_id=editors.id
                 JOIN librarys on library_id=librarys.id
                 JOIN author_book on book_id=books.id
-                JOIN authors on author_id=authors.id';
+                JOIN authors on author_id=authors.id
+                ORDER BY book_id';
+        $pdost = $this->cx->query($sql);
+        return $pdost->fetchAll();
+    }
+    public function paginate($page)
+    {
+
+        $page=NBR_BOOKS*($page-1);
+        
+         $sql = '
+                 SELECT
+                books.id AS book_id,
+                genres.id AS genre_id,
+                editors.id AS editor_id,
+                librarys.id AS library_id,
+                authors.id as author_id,
+                title,
+                front_cover,
+                summary,
+                authors.first_name AS author_first_name,
+                authors.last_name AS author_last_name,
+                editors.name AS editor_name,
+                genres.name AS genre_name,
+                datepub,
+                librarys.name AS library_name
+
+                FROM
+
+                books
+                JOIN genres ON genre_id=genres.id
+                JOIN editors on editor_id=editors.id
+                JOIN librarys on library_id=librarys.id
+                JOIN author_book on book_id=books.id
+                JOIN authors on author_id=authors.id
+                ORDER BY book_id
+                LIMIT '.NBR_BOOKS.' OFFSET '.$page;
         $pdost = $this->cx->query($sql);
         return $pdost->fetchAll();
     }
@@ -180,4 +217,5 @@ class Book extends Model implements BookRepositoryInterface
         $pdost->execute([':book_id' => $book_id]);
         return $pdost->fetch();
     }
+
 }
