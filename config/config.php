01d0fb6c680c905516f56e41241d7cdd284e62a1
Daniel <daniel.schreurs@hotmail.com>
2015-03-17 20:52:42 +0100

    rename CONSTANT

--- config/config.php
+++ config/config.php
@@ -44,7 +44,7 @@ define('LANGUAGES_DIR', 'languages/');
 define('HELPERS_DIR', 'helpers/');
 define('IMAGES_DIR', 'images/');
 define('CARBON_DIR', 'vendor/Carbon/');
-define('nbrBookPage', '2');
+define('NBR_BOOKS', 2);
 
 /*
  * Initialisation éventuelles de diverses variables comme par exemple,
