e0c0fc240fac3e3a5a7a60b6ac3cf110e334f246
Daniel <daniel.schreurs@hotmail.com>
2015-03-17 22:14:33 +0100

    fixe pagination

--- views/parts/pagination.php
+++ views/parts/pagination.php
@@ -1,10 +1,32 @@
+<?php $i = 1;
+$c = $data['nbrPage'];
+$current = $data['currentPage'];
+
+?>
 <ul class="pagination">
-    <li class="page_number"><span class="numberBtn large">1</span></li>
-    <li class="page_number"><a class="numberBtn" href="<?php echo($html->createLink('book','group')) ?>">2</a></li>
-    <li class="page_number"><a class="numberBtn" href="<?php echo($html->createLink('book','group')) ?>">3</a></li>
-    <li class="page_number"><a class="numberBtn" href="<?php echo($html->createLink('book','group')) ?>">4</a></li>
-    <li class="page_number"><a class="numberBtn" href="<?php echo($html->createLink('book','group')) ?>">5</a></li>
-    <li class="page_number"><span class="numberBtn"> ... </span></li>
-    <li class="page_number"><a class="numberBtn" href="<?php echo($html->createLink('book','group')) ?>0">10</a></li>
-    <li class="page_number"><a href="<?php echo($html->createLink('book','group')) ?>" class="numberBtn large">Suivant</a></li>
+    <?php if($current>1):?>
+        <li class="page_number">
+            <a  class="numberBtn large"
+                href="<?php echo($html->createLink('book', 'index', ['page' =>$current-1 ])) ?>">Précédent</a>
+        </li>
+    <?php endif; ?>
+    <?php for ($i = 1; $i <= $c; $i++): ?>
+        <?php if ($i ==$c): ?>
+            <li class="page_number"><span class="numberBtn"> ... </span></li>
+        <?php endif; ?>
+        <li class="page_number">
+            <a  class="numberBtn <?php echo($i==$current?'large':''); ?>"
+                href="<?php echo($html->createLink('book', 'index', ['page' => $i])) ?>">
+            <?php echo($i); ?></a>
+        </li>
+    <?php endfor ?>
+    <?php if($current<$c):?>
+        <li class="page_number">
+            <a  class="numberBtn large"
+                href="<?php echo($html->createLink('book', 'index', ['page' => $current+1])) ?>">Suivant</a>
+        </li>
+    <?php endif; ?>
+
+
+
 </ul>
