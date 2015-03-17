09fafe032fd629401996bd2485a8132819fbeed6
Daniel <daniel.schreurs@hotmail.com>
2015-03-17 08:28:01 +0100

    orthographe

--- views/editor/index.php
+++ views/editor/index.php
@@ -7,7 +7,7 @@
                 <h3><a href="<?php echo($html->createLink('editor','view',['id'=>$editor->editor_id])); ?>"><?php echo($editor->editor_name); ?></a></h3>
                 <a href="<?php echo($html->createLink('editor','view',['id'=>$editor->editor_id])); ?>"><img height="250" width="250" src="./img/editors_logos/<?php echo($editor->editor_logo) ?>.png" alt="logo de <?php echo($editor->editor_name); ?>"/></a>
                 <p><?php echo($html->cutText($editor->bio_text,200)); ?></p>
-                <a class="btnVert" href="<?php echo($html->createLink('editor','view',['id'=>$editor->editor_id])); ?>">Voir la fiche de l'éditeur</a>
+                <a class="btnVert" title="Afiche la fiche de l'éditeur" href="<?php echo($html->createLink('editor','view',['id'=>$editor->editor_id])); ?>">Voir la fiche de l'éditeur</a>
             </li>
         <?php endforeach; ?>
     </ul>
