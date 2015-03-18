1e39b3fc5558612613932865ec02fc4fd1a84d77
Daniel <daniel.schreurs@hotmail.com>
2015-03-18 20:47:03 +0100

    delete options

--- views/parts/form_recherche.php
+++ views/parts/form_recherche.php
@@ -1,28 +1,6 @@
-<form name="global" class="form_recherche">
-    <input type="search" list="tous" required placeholder="Un livre, un auteur" name="recherche" id="recherche">
-    <datalist id="tous">
-        <option value="Vincent Hein">Vincent Hein</option>
-        <option value="Joël Egloff">Joël Egloff</option>
-        <option value="Christophe Honoré">Christophe Honoré</option>
-        <option value="Thomas Paris">Thomas Paris</option>
-        <option value="Mamadou Mahmoud N'Dongo">Mamadou Mahmoud N'Dongo</option>
-        <option value="Anna Gavalda">Anna Gavalda</option>
-        <option value="Béatrice Leca">Béatrice Leca</option>
-        <option value="Johan Heliot">Johan Heliot</option>
-        <option value="Gwenaëlle Aubry">Gwenaëlle Aubry</option>
-        <option value="J.M. Erre">J.M. Erre</option>
-        <option value="Anne Godard">Anne Godard</option>
-        <option value="Samira Bellil">Samira Bellil</option>
-        <option value="Mathias Enard">Mathias Enard</option>
-        <option value="Sigrid Baffert">Sigrid Baffert</option>
-        <option value="Nicolas Fargues">Nicolas Fargues</option>
-        <option value="Laurent Gaudé">Laurent Gaudé</option>
-        <option value="Karelle Ménine">Karelle Ménine</option>
-        <option value="Valérie Mréjen">Valérie Mréjen</option>
-        <option value="Sylvain Tesson">Sylvain Tesson</option>
-        <option value="Roland Michel Tremblay">Roland Michel Tremblay</option>
-        <option value="Chloé Delaume">Chloé Delaume</option>
-    </datalist>
+<form name="global" class="form_recherche" method="post" action="<?php echo($html->createLink('page','search'));  ?>" >
+    <input type="search" list="tous" required placeholder="Un livre, un auteur" name="search" id="recherche">
+
     <select name="blibliotheque">
         <option value="Chiroux" title="Selectionner votre blibliotheque">LA HESTRE(Manage)</option>
         <option value="Chiroux" title="Selectionner votre blibliotheque">LA HULPE</option>
