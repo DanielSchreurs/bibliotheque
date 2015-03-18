ceabe97a6eec871d07f7603a02b26b4689b58745
Daniel <daniel.schreurs@hotmail.com>
2015-03-18 20:44:31 +0100

    add seach function

--- controllers/Page.php
+++ controllers/Page.php
@@ -7,22 +7,50 @@ use Models\Help;
 
 class Page extends Base
 {
-    function __construct (Request $request){
+    function __construct(Request $request)
+    {
         parent::__construct($request);
-        $this->Modelhelp= new Help();
+        $this->Modelhelp = new Help();
     }
 
 
     public function help()
     {
-            $date=$this->Modelhelp->getAllQuestions();
-            $title='Comment ça marche';
-            $view='help.php';
-            return [
-                'data'=>$date,
-                'title'=>$title,
-                'view'=>$view
-            ];
+        $date = $this->Modelhelp->getAllQuestions();
+        $title = 'Comment ça marche';
+        $view = 'help.php';
+        return [
+            'data' => $date,
+            'title' => $title
+        ];
+    }
+
+    public function search()
+    {
+        $data = $this->Modelhelp->searchALL([
+                'books' => [
+                    'get' => 'title, id',
+                    'where' => ['title','datepub'],
+                    'what' => $_POST['search']
+                ],
+                'authors' => [
+                    'get' => 'first_name, last_name, id',
+                    'where' => ['first_name', 'last_name'],
+                    'what' => $_POST['search']
+                ],
+                'editors' => [
+                    'get' => 'name, id',
+                    'where' => ['website', 'name'],
+                    'what' => $_POST['search']
+                ]
+            ]
+        );
+        $title= $_POST['search'];
+        return [
+            'data'=>$data,
+            'title'=>$title
+        ];
+
     }
 
 }
