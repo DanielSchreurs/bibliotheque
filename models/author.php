5d940ef963b37d6efca8d53e69c38b9c6b26b664
Daniel <daniel.schreurs@hotmail.com>
2015-03-18 21:52:44 +0100

    fixe because one author have more than one book

--- models/author.php
+++ models/author.php
@@ -51,7 +51,7 @@ class Author extends Model implements AuthorRepositoryInterface
                 WHERE author_id=:id';
         $pds = $this->cx->prepare($sql);
         $pds->execute([':id' => $id]);
-        return $pds->fetch();
+        return $pds->fetchAll();
     }
 
     function books($id)
