d3fe5804b7259aba81d791db47d6df762d5fefee
Daniel <daniel.schreurs@hotmail.com>
2015-03-18 11:34:21 +0100

    add sql

--- models/Help.php
+++ models/Help.php
@@ -15,6 +15,17 @@ class Help extends Model implements HelpRepositoryInterface
         parent::__construct($this->table);
     }
     function getAllQuestions(){
-        return[];
+        $sql='
+            SELECT
+            question,
+            answer,
+            first_name as user_first_name,
+            last_name as user_last_name
+
+            FROM questions
+            JOIN answers ON answer_id=answers.id
+            JOIN users ON questions.user_id= users.id';
+        $pdost=$this->cx->query($sql);
+        return $pdost->fetchAll();
     }
 }
