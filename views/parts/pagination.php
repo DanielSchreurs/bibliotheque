9e5a169d9018331877ab615ab5a5f6358d2bdbd0
Daniel <daniel.schreurs@hotmail.com>
2015-03-20 22:18:29 +0100

    Add bem

--- views/parts/pagination.php
+++ views/parts/pagination.php
@@ -1,29 +1,27 @@
 <?php $i = 1;
 $c = $data['nbrPage'];
 $current = $data['currentPage'];
-var_dump($current);
-
 ?>
-<ul class="pagination">
+<ul class="site-pagination">
     <?php if($current>1):?>
-        <li class="page_number">
-            <a  class="numberBtn large"
+        <li class="site-pagination__number">
+            <a  class="site-pagination__link--current"
                 href="<?php echo($html->createLink('book', 'index', ['page' =>$current-1 ])) ?>">Précédent</a>
         </li>
     <?php endif; ?>
     <?php for ($i = 1; $i <= $c; $i++): ?>
         <?php if ($c>3): ?>
-            <li class="page_number"><span class="numberBtn"> ... </span></li>
+            <li class="site-pagination__number"><span class="site-pagination__link"> ... </span></li>
         <?php endif; ?>
-        <li class="page_number">
-            <a  class="numberBtn <?php echo($i==$current?'large':''); ?>"
+        <li class="site-pagination__number">
+            <a  class=" <?php echo($i==$current?'site-pagination__link--current':'site-pagination__link'); ?>"
                 href="<?php echo($html->createLink('book', 'index', ['page' => $i])) ?>">
             <?php echo($i); ?></a>
         </li>
     <?php endfor ?>
     <?php if($current<$c):?>
-        <li class="page_number">
-            <a  class="numberBtn large"
+        <li class="site-pagination__number">
+            <a  class="site-pagination__link--current"
                 href="<?php echo($html->createLink('book', 'index', ['page' => $current+1])) ?>">Suivant</a>
         </li>
     <?php endif; ?>
