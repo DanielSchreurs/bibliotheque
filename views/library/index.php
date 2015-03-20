7e6d9592fed2e97cc239a6e1108ed0ca8b09ebf3
Daniel <daniel.schreurs@hotmail.com>
2015-03-20 22:17:53 +0100

    reformat

--- views/library/index.php
+++ views/library/index.php
@@ -2,18 +2,17 @@
 <main>
     <?php include('./views/parts/form_recherche.php'); ?>
     <?php include('./views/parts/main_nav_bibli.php'); ?>
+    <h2>Nos Bibliothèques</h2>
     <ul>
+        <li>
         <?php foreach ($data['data'] as $library): ?>
             <h3><a href="<?php echo $html->createLink('library', 'view',
-                    ['id' => $library->id]); ?>"><?php echo($library->name); ?></a></h3>
-            <h2><a href="<?php echo $html->createLink('library', 'view',
-                    ['id' => $library->id]); ?>"><?php echo($library->slogan) ?></a></h2>
+                    ['id' => $library->id]); ?>"><?php echo($library->slogan) ?></a></h3>
             <p><?php echo($library->phone); ?></p>
             <a href="<?php echo($html->createLink('library', 'view', ['id' => $library->id])); ?>"><img
                     src="./img/librarys/logo/<?php echo($library->logo); ?>.png"
                     alt="Logo de <?php echo($library->name); ?>"/></a>
         <?php endforeach; ?>
-        <li>
         </li>
     </ul>
 </main>
