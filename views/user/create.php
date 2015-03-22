78999b58621e545b506f3b93531327eb4aa9981f
Daniel <daniel.schreurs@hotmail.com>
2015-03-22 23:10:59 +0100

    rename for Bem

--- views/user/create.php
+++ views/user/create.php
@@ -1,23 +1,30 @@
+
 <main>
+  <h1>Créer un nouveau compte</h1>
   <h2>Faites partie de la communauté en quelques clics !</h2>
-  <form class="ceateUser" action="<?php $html->createLink('user','create'); ?>" method="post">
-    <p> Les champs précédés d’un <strong>(*)</strong> sont obligatoires!</p>
+  <form class="form-create-user">
+    <p> Les champs précédés d’un <strong class="form-create-user__obligatoire">(*)</strong> sont obligatoires!</p>
     <fieldset>
       <legend>Qui êtes vous ?</legend>
-      <label for="first_name">Votre prénom</label>
+      <label for="first_name">Votre prénom <strong class="form-create-user__obligatoire">*</strong></label>
       <input type="text" name="first_name" id="first_name" value="" placeholder="Jane" title="Introduisez votre prénom"/>
-      <label for="last_name">Votre nom de famille</label>
+      <label for="last_name">Votre nom de famille <strong class="form-create-user__obligatoire">*</strong></label>
       <input type="text" name="last_name" id="last_name" value="" placeholder="Doe" title="Introduisez votre nom de famillie"/>
+
+        <div class="form-create-user--error">
+            <p>Votre champ nom, prenom doivent être remplis.</p>
+        </div>
     </fieldset>
     <fieldset>
       <legend>Votre compte</legend>
-      <label for="username">Votre nom d'utilisateur</label>
+      <label for="username">Votre nom d'utilisateur <strong class="form-create-user__obligatoire">*</strong></label>
       <input type="text" name="username" id="username" value="" title="Introduisez votre nom d’utilisateur" placeholder="Janette"/>
-      <label for="email">Votre adresse mail</label>
-      <input type="email" name="email" id="email" value="" title="Introduisez votre adressse mail" placeholder="jane.doe@outlook.com"/><a id="linkShowPassword" href="#" title="Afficher les mots de pass" class="smallInfo icon ouvrir">Montrer les mots de pass</a>
-      <label for="password">Votre Mot de passes</label>
+      <label for="email">Votre adresse mail <strong class="form-create-user__obligatoire">*</strong></label>
+      <input type="email" name="email" id="email" value="" title="Introduisez votre adressse mail" placeholder="jane.doe@outlook.com"/>
+        <span id="linkShowPassword" title="Afficher les mots de pass" class="smallInfo icon ouvrir">Montrer les mots de pass</span>
+      <label for="password">Votre Mot de passes <strong class="form-create-user__obligatoire">*</strong></label>
       <input type="password" name="password" id="password" value="" title="Introduisez votre mot de pass" class="showedpassword"/>
-      <label for="password1">Votre Mot de passes</label>
+      <label for="password1">Votre Mot de passes <strong class="form-create-user__obligatoire">*</strong></label>
       <input type="password" name="password1" id="password1" value="" title="Introduisez une seconde fois votre mot de passe" class="showedpassword"/>
     </fieldset>
     <fieldset>
