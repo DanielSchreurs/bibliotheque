1f36d5d0dd630f801ef1da56f3338215252833f2
Daniel <daniel.schreurs@hotmail.com>
2015-03-25 22:23:01 +0100

    prepare add book

--- controllers/Book.php
+++ controllers/Book.php
@@ -2,30 +2,48 @@
 
 namespace Controllers;
 
+use Models\AuthorRepositoryInterface;
 use Models\BookRepositoryInterface as BookRepository;
+use Models\EditorRepositoryInterface;
+use Models\GenreRepositoryInterface;
+use Models\LibraryRepositoryInterface;
+
 
 class Book extends Base
 {
-    private $modelbook = null;
-    private $modellibrary = null;
+    private $modelBook = null;
+    private $modelAuthor = null;
+    private $modelEditor = null;
+    private $modelGenre = null;
+    private $modelLibrary = null;
 
 
-    public function __construct(Request $request, BookRepository $BookModel)
+    public function __construct(
+        Request $request,
+        BookRepository $modelBook,
+        AuthorRepositoryInterface $modelAuthor,
+        EditorRepositoryInterface $modelEditor,
+        GenreRepositoryInterface $modelGenre,
+        LibraryRepositoryInterface $modelLibrary)
     {
         parent::__construct($request);
-        $this->modelbook = $BookModel;
+        $this->modelbook = $modelBook;
+        $this->modelAuthor= $modelAuthor;
+        $this->modelEditor= $modelEditor;
+        $this->modelGenren= $modelGenre;
+        $this->modellibrary= $modelLibrary;
     }
 
     public function index()
     {
         $data = $this->modelbook->paginate($this->request->page);
         $title = 'acceuil';
-        $nbrPage= ceil(($this->modelbook->getNbrelements()/NBR_BOOKS));
+        $nbrPage = ceil(($this->modelbook->getNbrelements() / NBR_BOOKS));
         return [
             'data' => $data,
             'title' => $title,
-            'nbrPage'=>$nbrPage,
-            'currentPage'=>$this->request->page
+            'nbrPage' => $nbrPage,
+            'currentPage' => $this->request->page
         ];
 
     }
@@ -63,9 +81,17 @@ class Book extends Base
     }
 
 
-    public function edit()
+    public function create()
     {
-
+            $title='Ajouter une livre';
+            $data['authors']=$this->modelAuthor->all();
+            $data['editors']=$this->modelEditor->all();
+            $data['genres']=$this->modelGenre->all();
+            $data['librarys']=$this->modelLibrary->all();
+        return[
+            'data'=>$data,
+            'title'=>$title
+        ];
     }
 
 
