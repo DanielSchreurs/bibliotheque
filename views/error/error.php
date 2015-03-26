938e4af86cdc1cedf957bd6b9dc74cef71237593
Daniel <daniel.schreurs@hotmail.com>
2015-03-26 23:50:20 +0100

    init file

---
+++ views/error/error.php
@@ -0,0 +1,4 @@
+<main>
+    <h2><a href="<?php echo($html->createLink('book','index')) ?>" title="Vous renvois vers la page d'acceuil"><?php echo($data['title']); ?></a></h2>
+    <a href="<?php echo($html->createLink('book','index')) ?>" title="Vous renvois vers la page d'acceuil"> Retour à la page d'acceuil</a>
+</main>
