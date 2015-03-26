0626bb437003792917f3219e9e69798189cf0e3a
Daniel <daniel.schreurs@hotmail.com>
2015-03-26 23:49:52 +0100

    init file

--- views/page/help.php
+++ views/page/help.php
@@ -2,7 +2,7 @@
     <?php include('./views/parts/form_recherche.php'); ?>
     <?php include('./views/parts/main_nav_bibli.php'); ?>
     <h2>Comment ça marche&nbsp;?</h2>
-    <?php foreach($data['data'] as $article): ?>
+    <?php foreach ($data['data'] as $article): ?>
         <article class="question">
             <h3 class="article__title"><?php echo($article->question); ?></h3>
             <blockquote class="big__blockquote">
--- views/user/create.php
+++ views/user/create.php
@@ -1,18 +1,28 @@
 <main>
     <h1>Créer un nouveau compte</h1>
 
-    <h2>Faites partie de la communauté en quelques clics !</h2>
-    <?php $sent=$data['data']['sent']; var_dump($data); ?>
+    <h2><?php echo(isset($data['data']['message'])?$data['data']['message']:'Faites partie de la communauté en quelques clics !'); ?></h2>
+    <?php
+    isset($data['data']['sent']) ? $sent = $data['data']['sent'] : '';
+    isset($data['data']['errors']) ? $error = $data['data']['errors'] : '';
+    ?>
+    <?php if (!isset($data['data']['message'])): ?>
     <form class="form-create-user" action="<?php $html->createLink('user', 'create'); ?>" method="post">
         <p> Les champs précédés d’un <strong class="form-create-user__obligatoire">(*)</strong> sont obligatoires!</p>
         <fieldset>
             <legend>Qui êtes vous ?</legend>
             <label for="first_name">Votre prénom <strong class="form-create-user__obligatoire">*</strong></label>
-            <input class="error" type="text" name="first_name" id="first_name" value="<?php echo(isset($sent->first_name)?$sent->first_name:''); ?> " placeholder="Jane"
-                   title="Introduisez votre prénom"/>
+            <input <?php echo(isset($error['first_name']) ? 'class="error"' : ''); ?> type="text" name="first_name"
+                                                                                      id="first_name"
+                                                                                      value="<?php echo(isset($sent->first_name) ? $sent->first_name : ''); ?> "
+                                                                                      placeholder="Jane"
+                                                                                      title="Introduisez votre prénom"/>
             <label for="last_name">Votre nom de famille <strong class="form-create-user__obligatoire">*</strong></label>
-            <input type="text" name="last_name" id="last_name" value="" placeholder="Doe"
-                   title="Introduisez votre nom de famillie"/>
+            <input <?php echo(isset($error['last_name']) ? 'class="error"' : ''); ?> type="text" name="last_name"
+                                                                                     id="last_name"
+                                                                                     value="<?php echo(isset($sent->last_name) ? $sent->last_name : ''); ?>"
+                                                                                     placeholder="Doe"
+                                                                                     title="Introduisez votre nom de famillie"/>
 
             <!-- <div class="form-create-user--error">
                  <p>Votre champ nom, prenom doivent être remplis.</p>
@@ -22,15 +32,23 @@
             <legend>Votre compte</legend>
             <label for="username">Votre nom d'utilisateur <strong
                     class="form-create-user__obligatoire">*</strong></label>
-            <input type="text" name="username" id="username" value="" title="Introduisez votre nom d’utilisateur"
-                   placeholder="Janette"/>
+            <input <?php echo(isset($error['username']) ? 'class="error"' : ''); ?> type="text" name="username"
+                                                                                    id="username"
+                                                                                    value="<?php echo(isset($sent->username) ? $sent->username : ''); ?>"
+                                                                                    title="Introduisez votre nom d’utilisateur"
+                                                                                    placeholder="Janette"/>
             <label for="email">Votre adresse mail <strong class="form-create-user__obligatoire">*</strong></label>
-            <input type="text" name="email" id="email" value="" title="Introduisez votre adressse mail"
-                   placeholder="jane.doe@outlook.com"/>
+            <input <?php echo(isset($error['email']) ? 'class="error"' : ''); ?> type="email" name="email" id="email"
+                                                                                 value="<?php echo(isset($sent->email) ? $sent->email : ''); ?>"
+                                                                                 title="Introduisez votre adressse mail"
+                                                                                 placeholder="jane.doe@outlook.com"/>
             <span id="linkShowPassword" title="Afficher les mots de pass" class="smallInfo icon ouvrir">Montrer le mot de pass</span>
             <label for="password">Votre Mot de passes <strong class="form-create-user__obligatoire">*</strong></label>
-            <input type="password" name="password" id="password" value="" title="Introduisez votre mot de pass"
-                   class="showedpassword"/>
+            <input <?php echo(isset($error['password']) ? 'class="error showedpassword"' : 'class="showedpassword"'); ?>
+                type="password" name="password" id="password"
+                value="<?php echo(isset($sent->password) ? $sent->password : ''); ?>"
+                title="Introduisez votre mot de pass"
+                class="showedpassword"/>
             <!-- <div class="form-create-user--error">
                  <p>Votre champ nom, prenom doivent être remplis.</p>
 
@@ -39,10 +57,14 @@
         <fieldset>
             <legend>Sécurité</legend>
             <label for="question">Question de sécurité</label>
-            <input type="text" name="question" id="question" value="" title="Introduisez une question de sécurité"
+            <input type="text" name="question" id="question"
+                   value="<?php echo(isset($sent->question) ? $sent->question : ''); ?>"
+                   title="Introduisez une question de sécurité"
                    placeholder="Quel est le nom de mon premier copain"/>
             <label for="answer">réponse à cette question</label>
-            <input type="text" name="answer" id="answer" value="" title="Introduisez la réponse à la question"
+            <input type="text" name="answer" id="answer"
+                   value="<?php echo(isset($sent->answer) ? $sent->answer : ''); ?>"
+                   title="Introduisez la réponse à la question"
                    placeholder="Mystère"/>
             <!-- <div class="form-create-user--error">
                  <p>Votre champ nom, prenom doivent être remplis.</p>
@@ -50,4 +72,8 @@
         </fieldset>
         <input type="submit" value="Soumettre l'inscription" class="btnVert"/>
     </form>
+
+    <?php else: ?>
+        <a class="btnVert" href="<?php echo($html->createLink('book','index')); ?>"> Retour à l'accueil</a>
+    <?php endif; ?>
 </main>
