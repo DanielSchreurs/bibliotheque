f41a6a4688fab3c96216d62651324557f231d110
Daniel <daniel.schreurs@hotmail.com>
2015-03-29 17:51:35 +0200

    reformat

--- controllers/Base.php
+++ controllers/Base.php
@@ -6,12 +6,11 @@ class Base
 {
     public $view = null;
     public $request = null;
+    public $message = null;
 
     function __construct(Request $request)
     {
         $this->view = $request->m . '/' . $request->a . '.php';
         $this->request = $request;
     }
-
-
 }
