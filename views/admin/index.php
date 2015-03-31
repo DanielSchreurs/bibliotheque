f3bf7fced909234fe8a7017f46d3c8ebf2f562f5
Daniel <daniel.schreurs@hotmail.com>
2015-03-31 23:21:34 +0200

    remove  this function

--- views/admin/index.php
+++ views/admin/index.php
@@ -3,7 +3,6 @@
   <h2>Administrer son site, en quelques clics</h2>
 <nav class="main-nav">
     <h2 class="hidden">Navigattion des liens d'administration</h2>
-    <a class="main-nav__item--small" href="<?php echo($html->createLink('book','admin')); ?>" title="">Administrer les livres</a>
     <a class="main-nav__item--small" href="<?php echo($html->createLink('author','admin')); ?>" title="">Administrer les auteurs</a>
     <a class="main-nav__item--small" href="<?php echo($html->createLink('genre','admin')); ?>" title="">Administrer les genres</a>
     <a class="main-nav__item--small" href="<?php echo($html->createLink('user','admin')); ?>" title="">Administrer les utilisateurs</a>
