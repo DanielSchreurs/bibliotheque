352a47df4064367303f720bd7dcb754bf39d0c98
Daniel <daniel.schreurs@hotmail.com>
2015-03-31 23:21:11 +0200

    fixed conexion

--- models/model.php
+++ models/model.php
@@ -11,7 +11,7 @@ class model implements ModelRepositoryInterface
         global $options;
         //Je me connecte au SGBD
         try {
-            $this->cx = new \PDO(DSN, USERNAME, PASSWORD, $options);
+            $this->cx= new \PDO(DSN,USERNAME,PASSWORD,$options);
             $this->cx->query('SET CHARACTER SET UTF8');
             $this->cx->query('SET NAMES UTF8');
         } catch (\PDOException $e) {
