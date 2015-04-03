4d38b6a601ece333e96b1ce590ca886299251825
Daniel <daniel.schreurs@hotmail.com>
2015-04-03 23:56:21 +0200

    for admin view

--- controllers/Base.php
+++ controllers/Base.php
@@ -2,14 +2,22 @@
 
 namespace Controllers;
 
+use Components\Session;
+
 class Base
 {
     public $view = null;
     public $request = null;
+    public $isAdmin = false;
 
     function __construct(Request $request)
     {
         $this->view = $request->m . '/' . $request->a . '.php';
         $this->request = $request;
+        $name = explode('_', $this->request->a);
+        if (Session::isAdmin() && $name[0] == 'admin') {
+            $this->isAdmin = true;
+            $this->view = $request->m . '/admin/' . $name[1] . '.php';
+        }
     }
 }
