915f56a23adddcc952485912e4435b4aad54dcec
Daniel <daniel.schreurs@hotmail.com>
2015-04-09 00:58:54 +0200

    add form

---
+++ views/book/admin/create.php
@@ -0,0 +1,128 @@
+<main class="container">
+    <?php Components\Session::flash(); ?>
+    <h1 class="header-block-one"><?php echo($data['title']); ?></h1>
+    <?php
+    $authors = $data['data']['authors'];
+    $editors = $data['data']['editors'];
+    $genres = $data['data']['genres'];
+    $langues = $data['data']['langues'];
+    isset($data['data']['errors']) ? $errors = $data['data']['errors'] : '';
+    isset($data['data']['sent']) ? $sent = $data['data']['sent'] : '';
+    $_GET = $_SERVER['REQUEST_METHOD'] == 'GET';
+    ?>
+    <form class="form-create large"
+          action="<?php echo($html->createLink('book', 'admin_create_book')); ?>" method="post"
+          enctype="multipart/form-data">
+        <p class="form-create__infos"> Les champs précédés d’un <strong
+                class="form-create--obligatoire">(*)</strong> sont obligatoires!</p>
+
+        <label for="title">Titre du livre<strong
+                class="form-create--obligatoire">*</strong></label>
+        <input type="text"
+               class="form-create__simple-imput <?php echo(isset($errors['title'])? 'error' :''); ?>"
+               name="title"
+               id="title"
+               value="<?php echo(isset($errors['title']) || $_GET ? '' : $sent->title); ?>"
+               placeholder="Titre du livre"
+               title="Introduisez le titre de votre livre"/>
+        <?php if (isset($errors['title'])): ?>
+            <p class="form-create__message--error"><?php echo($errors['title']); ?><span class="flash-box__btn">X</span>
+            </p>
+        <?php endif; ?>
+        <label for="author_id">Auteur du livre</label>
+        <select class="form-create__select" name="author_id" id="author_id">
+            <?php foreach ($authors as $author): ?>
+                <option
+                    <?php echo(isset($sent->author_id) && $sent->author_id == $sent->author_id ? 'selected' : ''); ?>
+                    value="<?php echo($author->author_id); ?> "><?php echo($author->first_name . ' ' . $author->last_name); ?></option>
+            <?php endforeach; ?>
+        </select>
+        <label for="language_id">La langue du livre</label>
+        <select class="form-create__select" name="language_id" id="language_id">
+            <?php foreach ($langues as $langue): ?>
+                <option
+                    <?php echo(isset($sent->language_id) && $sent->language_id == $sent->language_id ? 'selected' : ''); ?>
+                    value="<?php echo($langue->language_id); ?> "><?php echo($langue->language); ?></option>
+            <?php endforeach; ?>
+        </select>
+        <label for="editor_id">Editeur du livre</label>
+        <select class="form-create__select" name="editor_id" id="editor_id">
+            <?php foreach ($editors as $editor): ?>
+                <option
+                    <?php echo(isset($sent->editor_id) && $sent->editor_id == $sent->editor_id ? 'selected' : ''); ?>
+                    value="<?php echo($editor->editor_id); ?> "><?php echo($editor->editor_name); ?></option>
+            <?php endforeach; ?>
+        </select>
+        <label for="genre_id">Genre du livre</label>
+        <select class="form-create__select" name="genre_id" id="genre_id">
+            <?php foreach ($genres as $genre): ?>
+                <option
+                    <?php echo(isset($sent->editor_id) && $sent->editor_id == $sent->genre_id ? 'selected' : ''); ?>
+                    value="<?php echo($genre->id); ?> "><?php echo($genre->name); ?></option>
+            <?php endforeach; ?>
+        </select>
+        <label for="front_cover">Couverture du livre 300/450px<strong
+                class="form-create--obligatoire">*</strong></label>
+        <input class="form-create__simple-imput <?php echo(isset($errors['front_cover'])? 'error' :''); ?>" type="file" name="front_cover" id="front_cover"
+               title="Chargez le couverture de votre livre"/>
+        <?php if (isset($errors['front_cover'])): ?>
+            <p class="form-create__message--error">
+                <?php foreach ($errors['front_cover'] as $error): ?>
+                    <?php echo($error); ?>
+                <?php endforeach; ?>
+
+                <span class="flash-box__btn">X</span></p>
+        <?php endif; ?>
+        <label for="front_cover_presentation">Petite couverture 270/200px<strong
+                class="form-create--obligatoire">*</strong></label>
+        <input class="form-create__simple-imput <?php echo(isset($errors['front_cover_presentation'])? 'error' :''); ?>" type="file" name="front_cover_presentation"
+               id="front_cover_presentation"
+               title="Chargez le couverture de votre livre"/>
+        <?php if (isset($errors['front_cover_presentation'])): ?>
+            <p class="form-create__message--error">
+                <?php foreach ($errors['front_cover_presentation'] as $error): ?>
+                    <?php echo($error); ?>
+                <?php endforeach; ?>
+
+                <span class="flash-box__btn">X</span></p>
+        <?php endif; ?>
+        <label for="summary">Résumé du livre<strong
+                class="form-create--obligatoire">*</strong></label>
+        <textarea class="form-create__long-text <?php echo(isset($errors['summary'])? 'error' :''); ?>" name="summary" id="summary" cols="30"
+                  rows="10"><?php echo(isset($errors['summary']) || $_GET ? '' : $sent->summary); ?></textarea>
+        <?php if (isset($errors['summary'])): ?>
+            <p class="form-create__message--error"><?php echo($errors['summary']); ?><span
+                    class="flash-box__btn">X</span></p>
+        <?php endif; ?>
+        <label for="isbn">ISBN du livre<strong
+                class="form-create--obligatoire">*</strong></label>
+        <input class="form-create__simple-imput <?php echo(isset($errors['isbn'])? 'error' :''); ?>" type="text" name="isbn" id="isbn"
+               title="Introduisez votre ISBN"
+               value="<?php echo(isset($errors['isbn']) || $_GET ? '' : $sent->isbn); ?>"/>
+        <?php if (isset($errors['isbn'])): ?>
+            <p class="form-create__message--error"><?php echo($errors['isbn']); ?><span class="flash-box__btn">X</span>
+            </p>
+        <?php endif; ?>
+        <label for="nbpages">Nombre de page<strong
+                class="form-create--obligatoire">*</strong></label>
+        <input class="form-create__simple-imput <?php echo(isset($errors['nbpages'])? 'error' :''); ?>" type="number" name="nbpages" id="nbpages"
+               title="Introduisez le nombre de page du livre"
+               value="<?php echo(isset($errors['nbpages']) || $_GET? '' : $sent->nbpages); ?>"/>
+        <?php if (isset($errors['nbpages'])): ?>
+            <p class="form-create__message--error"><?php echo($errors['nbpages']); ?><span
+                    class="flash-box__btn">X</span></p>
+        <?php endif; ?>
+        <label for="datepub">Date de publication (jj/mm/aaaa)<strong
+                class="form-create--obligatoire">*</strong></label>
+        <input class="form-create__simple-imput <?php echo(isset($errors['datepub'])? 'error' :''); ?>" type="date" name="datepub" min="2" id="datepub"
+               title="Introduisez la date de publication" placeholder="12/03/2014"
+               value="<?php echo(isset($errors['datepub']) ||$_GET ? '' : $sent->datepub); ?>"/>
+        <?php if (isset($errors['datepub'])): ?>
+            <p class="form-create__message--error"><?php echo($errors['datepub']); ?><span
+                    class="flash-box__btn">X</span></p>
+        <?php endif; ?>
+        <label for="vedette">Mettre en vedette</label>
+        <input type="checkbox" name="vedette" id="vedette" value="1"/>
+        <input type="submit" value="Modifier le livre" class="btnVert"/>
+    </form>
+</main>
