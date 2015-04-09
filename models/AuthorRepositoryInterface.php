16766a8c05c79ff723884bab4283e246749aa0d4
Daniel <daniel.schreurs@hotmail.com>
2015-04-10 00:48:31 +0200

    add function delete, edit, add

--- models/AuthorRepositoryInterface.php
+++ models/AuthorRepositoryInterface.php
@@ -15,11 +15,15 @@ interface AuthorRepositoryInterface
 
     function find($id);
 
-
+    function getAuthor($id);
     function books($id);
 
     public function getAuthorMonth();
 
     public function getAllName();
+
+    public function update($authorObj, $author_id);
+
+    public function create($authorObj);
 
 }
