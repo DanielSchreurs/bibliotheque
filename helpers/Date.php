a24a44e1847049171514b6d45ca4d4c457b84339
Daniel <daniel.schreurs@hotmail.com>
2015-04-22 10:48:54 +0200

    add date function

--- helpers/Date.php
+++ helpers/Date.php
@@ -11,10 +11,10 @@ use Carbon\Carbon;
 
 class Date
 {
-    public static function isNotTolate($Y,$M,$D)
+    public static function isNotTolate($Y, $M, $D)
     {
-        $first=Carbon::create($Y,$M,$D);
-        $second=Carbon::now();
+        $first = Carbon::create($Y, $M, $D);
+        $second = Carbon::now();
         return $first->lte($second);
     }
 
@@ -23,32 +23,32 @@ class Date
         return preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $sting);
     }
 
-    public static function isValidDate($string,$mustBePassed=true){
-        if(!self::isValiddateFormat($string)){
-            return 'le format n’est pas bon';
-        }
-        else{
-            if($mustBePassed){
-                $date= explode('-', $string);
+    public static function isValidDate($string, $mustBePassed = true)
+    {
+        if (!self::isValiddateFormat($string)) {
+            return 'Oups, le format n’est pas bon';
+        } else {
+            if ($mustBePassed) {
+                $date = explode('-', $string);
 
-                if(!self::isNotTolate($date[0], $date[1], $date[2])) {
-                    return 'La date doit être dans le passé';
+                if (!self::isNotTolate($date[0], $date[1], $date[2])) {
+                    return 'Oups, la date doit être dans le passé';
+                }
+            } else {
+                $date = explode('-', $string);
+                if (self::isNotTolate($date[0], $date[1], $date[2])) {
+                    return 'Oups, la date doit être dans le futur';
                 }
             }
-            else{
-            $date= explode('-', $string);
-            if(self::isNotTolate($date[0], $date[1], $date[2])) {
-                return 'La date doit être dans le futur';
-            }
-            }
+
         }
         return true;
-        
+
     }
 
-    public static function getAge($birth, $death,$format='y')
+    public static function getAge($birth, $death, $format = 'y')
     {
         $interval = date_diff(date_create($birth), date_create($death));
-        return $interval->format('%'.$format);
+        return $interval->format('%' . $format);
     }
 }
