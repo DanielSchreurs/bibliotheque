57b897174afa60b8b8c1ad5ad6204a88922d3994
Daniel <daniel.schreurs@hotmail.com>
2015-04-22 10:46:00 +0200

    add a condition for user

--- controllers/Base.php
+++ controllers/Base.php
@@ -22,5 +22,13 @@ class Base
         elseif(!Session::isAdmin() && $name[0] == 'admin'){
             header('Location:./index.php?m=error&a=error&error=404');
         }
+        if (Session::isUserLogged() && $name[0] == 'user') {
+            $this->view = $request->m . '/user/' . $name[1] . '.php';
+        }
+        elseif (!Session::isUserLogged() && $name[0] == 'user') {
+            Session::setMessage('Oups, vous devez vous connecter, pour cette tâche.','error');
+            header('Location:' . $_SERVER['PHP_SELF']);
+            die();
+        }
     }
 }
