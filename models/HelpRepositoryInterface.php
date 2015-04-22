116b6eedd1cbb5b1e57b7af24787a86190060308
Daniel <daniel.schreurs@hotmail.com>
2015-04-22 23:46:20 +0200

    add function

--- models/HelpRepositoryInterface.php
+++ models/HelpRepositoryInterface.php
@@ -12,4 +12,19 @@ interface HelpRepositoryInterface
 
     function getAllQuestions();
 
+    function getQuestionAndAnswer($question_id);
+
+    public function getQuestion($id_question);
+
+    public function getAnswer($id_question);
+
+    public function createAnwser($oAnwser);
+
+    public function createQuestion($oQuestion);
+
+    public function deleteQuestion($question_id);
+    public function deleteAnswer($question_id);
+
+
+
 }
