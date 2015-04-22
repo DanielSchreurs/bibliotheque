6f7b0f4bf66bf27517f04d2d1cb8627fa3e0a109
Daniel <daniel.schreurs@hotmail.com>
2015-04-22 23:48:03 +0200

    add a new btn

--- views/parts/main_nav_admin.php
+++ views/parts/main_nav_admin.php
@@ -2,4 +2,5 @@
     <a class="main-nav__item--small" href="<?php echo($html->createLink('author','admin_index_author')); ?>" title="">Administrer les auteurs</a>
     <a class="main-nav__item--small" href="<?php echo($html->createLink('editor','admin_index_editor')); ?>" title="">Administrer les éditeurs</a>
     <a class="main-nav__item--small" href="<?php echo($html->createLink('genre','admin_index_genre')); ?>" title="">Administrer les genres</a>
+    <a class="main-nav__item--small" href="<?php echo($html->createLink('page','admin_indexHelp')); ?>" title="">Administrer les questions/réponses</a>
 </div>
