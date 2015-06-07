1cd5edddff24f7f8082764d4c9c0eb749f17275d
Daniel <daniel.schreurs@hotmail.com>
2015-06-07 10:57:48 +0200

    fixe pagination

--- views/parts/pagination.php
+++ views/parts/pagination.php
@@ -1,7 +1,6 @@
 <?php $i = 1;
 $c = $data['nbrPage'];
 $current = $data['currentPage'];
-var_dump($current, $c);
 ?>
 <ul class="site-pagination">
     <?php if ($current > 1): ?>
@@ -14,7 +13,7 @@ var_dump($current, $c);
     <?php for ($i = $current == 1 ? 1 : $current; $i <= $c; $i++): ?>
         <?php if ($c > MAX_NB_PAGE && $i == $c): ?>
             <li class="site-pagination__number"><span class="site-pagination__link--nohover"> ... </span></li>
-        <?php elseif ($i < ($current+MAX_NB_PAGE) ): ?>
+        <?php elseif (($i < $current+MAX_NB_PAGE )||($current+MAX_NB_PAGE>$i)): ?>
             <li class="site-pagination__number">
                 <a class=" <?php echo($i == $current ? 'site-pagination__link--current' : 'site-pagination__link'); ?>"
                    href="<?php echo($html->createLink('book', 'index', ['page' => $i])) ?>">
